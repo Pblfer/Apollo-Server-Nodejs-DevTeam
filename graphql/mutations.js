83c28ce5b1aa33681d433a0c18d4045c738ce32b
Pblfer <pblfertec@gmail.com>
2019-12-03 14:23:29 -0600

    Edit Apartament Mutation

--- graphql/mutations.js
+++ graphql/mutations.js
@@ -899,6 +899,23 @@ module.exports = {
     };
   },
 
+  updateApartament: async (root, { ID, objectField, value }) => {
+    let db;
+    let updateData;
+    try {
+      db = await connectDB();
+      updateData = await db
+        .collection("apartaments")
+        .updateOne({ _id: ObjectID(ID) }, { $set: { [objectField]: value } });
+    } catch (error) {
+      throw error;
+    }
+    return {
+      objectField,
+      value
+    };
+  },
+
   updateLevel: async (root, { levelID, objectField, value }) => {
     let db;
     let updateData;
