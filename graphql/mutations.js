e82642dda0156b1b6ba88b05a7ac2d7623614042
Pblfer <pblfertec@gmail.com>
2020-03-18 10:44:51 -0600

    SES TEST

--- graphql/mutations.js
+++ graphql/mutations.js
@@ -1058,7 +1058,7 @@ module.exports = {
       const options = {
         hostname: "dev.flattlo.com",
         port: 443,
-        path: "/webhook/7/webhook/email-flattlo",
+        path: "/webhook-test/7/webhook/email-flattlo",
         method: "POST",
         headers: {
           "Content-Type": "application/json"
