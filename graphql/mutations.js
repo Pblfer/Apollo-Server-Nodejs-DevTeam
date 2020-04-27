c0dbd70e8e5b1c7e1af2e9506df4bc43d3fa974e
Pblfer <pblfertec@gmail.com>
2020-04-27 10:50:19 -0600

    Edit Int, String, Double Pre Union

--- graphql/mutations.js
+++ graphql/mutations.js
@@ -1689,6 +1689,54 @@ module.exports = {
     };
   },
 
+  editProyectStringData: async (root, { ID, objectField, value }) => {
+    let db;
+    let updateData;
+    try {
+      db = await connectDB();
+      updateData = await db
+        .collection("proyects")
+        .updateOne({ _id: ObjectID(ID) }, { $set: { [objectField]: value } });
+    } catch (error) {
+      throw error;
+    }
+    return {
+      updateData
+    };
+  },
+
+  editProyectIntData: async (root, { ID, objectField, value }) => {
+    let db;
+    let updateData;
+    try {
+      db = await connectDB();
+      updateData = await db
+        .collection("proyects")
+        .updateOne({ _id: ObjectID(ID) }, { $set: { [objectField]: value } });
+    } catch (error) {
+      throw error;
+    }
+    return {
+      updateData
+    };
+  },
+
+  editProyectFloatData: async (root, { ID, objectField, value }) => {
+    let db;
+    let updateData;
+    try {
+      db = await connectDB();
+      updateData = await db
+        .collection("proyects")
+        .updateOne({ _id: ObjectID(ID) }, { $set: { [objectField]: value } });
+    } catch (error) {
+      throw error;
+    }
+    return {
+      updateData
+    };
+  },
+
   updateFlattloProfile: async (root, { ID, objectField, value }) => {
     let db;
     let updateData;
