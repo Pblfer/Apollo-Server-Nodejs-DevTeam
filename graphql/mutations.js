7ed929f40c88066362eb2260b3c57eb7b02b72f7
Pblfer <pblfertec@gmail.com>
2020-04-27 19:51:12 -0600

    Discount mutation fix

--- graphql/mutations.js
+++ graphql/mutations.js
@@ -2017,29 +2017,6 @@ module.exports = {
     } catch (error) {}
   },
 
-  deleteDiscountOnProyect: async (root, { proyectID, financingID }) => {
-    let db;
-    let deleteFinancing;
-    let removeFromProyect;
-    try {
-      db = await connectDB();
-      deleteFinancing = await db
-        .collection("financing_types")
-        .deleteOne({ _id: ObjectID(financingID) });
-    } catch (error) {
-      throw error;
-    }
-    try {
-      db = await connectDB();
-      removeFromProyect = await db
-        .collection("proyects")
-        .updateOne(
-          { _id: ObjectID(proyectID) },
-          { $pull: { financing_types: ObjectID(financingID) } }
-        );
-    } catch (error) {}
-  },
-
   deleteDiscountToProyect: async (root, { proyectID, discountID }) => {
     let db;
     let deleteDiscount;
